e401e500c5d9ba61c999a8c33bb21417906b7bd6
Kevin Lau <thekevlau@gmail.com>
2015-08-21 11:36:29 -0400

    Previous iteration was rendering the wrong copy of index.html; have deleted the old one since and kept the one under templates.

--- __init__.py
+++ __init__.py
@@ -6,22 +6,23 @@ monkey.patch_all()
 
 from flask import Flask
 from flask import redirect, render_template, url_for, jsonify, request, make_response
-from flask.ext.socketio import SocketIO
+from flask.ext.socketio import SocketIO, send, emit
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+m = {
+    "message": "some string"
+}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @socketio.on('my event')
 def handle_my_custom_event(json):
-    """does this even go anywhere?"""
-    print('received json: ' + str(json))
+    emit('my response', m)
 
 if __name__ == '__main__':
     socketio.run(app)
