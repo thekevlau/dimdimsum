4dabbbb3a6d1edeb86ddcd77999fea82e8a9bb82
Kevin Lau <thekevlau@gmail.com>
2015-08-23 00:19:16 -0400

    Reorganized app files to make things seem more flask-esque, per 'the flask mega tutorial' currently being followed.

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,21 @@
 """
 WSGI webapp using Flask
 """
-from gevent import monkey
+"""from gevent import monkey
 monkey.patch_all()
-
+"""
 from flask import Flask
 from flask import redirect, render_template, url_for, jsonify, request, make_response
 from flask.ext.socketio import SocketIO, send, emit
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+from app import views
+
 m = {
     "message": "some string"
 }
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+#app.config['SECRET_KEY'] = 'secret!'
 @socketio.on('my event')
 def handle_my_custom_event(json):
     """does this even go anywhere?"""
@@ -27,11 +23,16 @@ def handle_my_custom_event(json):
     print('received message: ' + json['data'])
     emit('my response', m)
 
+
+
+
+
+
 """
 @socketio.on('my event')
 def handle_my_custom_event(json):
     emit('my response', m)
 """
 
-if __name__ == '__main__':
-    socketio.run(app)
+#if __name__ == '__main__':
+#    socketio.run(app)
