d5f442b01cb960c18c291f82f800a4978469ab99
Shahmeer Navid <shahmeernavid@gmail.com>
2015-09-09 03:26:50 -0400

    Add join and create component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { Route, DefaultRoute, Redirect } from 'react-router';
 
 import App from './App';
+import Create from './components/Create';
 import Home from './components/Home';
+import Join from './components/Join';
 
 export default (
     <Route name="app" handler={App} path="/">
         <DefaultRoute handler={Home} />
+        <Route name="join" path="join" handler={Join} />
+        <Route name="create" path="create" handler={Create} />
     </Route>
 );
---
+++ src/components/Join.jsx
@@ -0,0 +1,12 @@
+import React from 'react/addons';
+import Router from 'react-router';
+
+export default React.createClass({
+    render: function() {
+        return (
+            <div>
+                Join
+            </div>
+        );
+    }
+});
